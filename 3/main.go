dfc95c5de22ea34b5db324486d11c0ae34498778
Jan Neumann <janneumannnneisen@gmail.com>
2024-12-04 20:19:20 +0100

    [Day 3] + Solved Assignment 2/2 for day 3

--- 3/main.go
+++ 3/main.go
@@ -13,7 +13,7 @@ func main() {
 	var filePath string = "/home/janneumann/Dokumente/Daten/Projekte/Privat/advent-of-code-2024/3/input.txt"
 
 	var data string = read_file(filePath)
-	var multiplyCalls []string = filterCorrectMultiplyCalls(data)
+	var multiplyCalls []string = filterMultiplyCalls(data)
 	var result int = 0
 	for i := range multiplyCalls {
 		var multiplicant int
@@ -23,6 +23,9 @@ func main() {
 	}
 
 	fmt.Println("Result Assignment 1: ", result)
+	var result2 int = assignmentTwo(data)
+	fmt.Println("Result Assignment 2: ", result2)
+
 }
 
 func multiply(multiplicant int, multiplicator int) int {
@@ -49,8 +52,41 @@ func getMultiplyPairs(value string) (int, int) {
 	return first, second
 }
 
-func filterCorrectMultiplyCalls(data string) []string {
-	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+func assignmentTwo(data string) int {
+	var matches []string = filterMatches(data)
+	fmt.Println(matches)
+	var allowedMultiplications []string = filterAllowed(matches)
+	var result int
+	for i := range allowedMultiplications {
+		var multiplicant int
+		var multiplicator int
+		multiplicant, multiplicator = getMultiplyPairs(allowedMultiplications[i])
+		result += multiply(multiplicant, multiplicator)
+	}
+	return result
+}
+
+func filterAllowed(matches []string) []string {
+	var allowed bool = true
+	var result []string
+	for i := range matches {
+		if matches[i] == "do()" {
+			allowed = true
+		} else if matches[i] == "don't()" {
+			allowed = false
+		} else {
+			if allowed {
+				result = append(result, matches[i])
+			} else {
+				continue
+			}
+		}
+	}
+	return result
+}
+
+func runRegEx(data string, regex string) []string {
+	re := regexp.MustCompile(regex)
 
 	matches := re.FindAllString(data, -1)
 
@@ -59,6 +95,14 @@ func filterCorrectMultiplyCalls(data string) []string {
 		result = append(result, match)
 	}
 	return result
+}
+
+func filterMultiplyCalls(data string) []string {
+	return runRegEx(data, "mul\\((\\d{1,3}),(\\d{1,3})\\)")
+}
+
+func filterMatches(data string) []string {
+	return runRegEx(data, "mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
 }
 
 func read_file(filePath string) string {
