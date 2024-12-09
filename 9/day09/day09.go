c66e6373283d008b25a7eca858ce8a1979655fe0
Jan Neumann <janneumannnneisen@gmail.com>
2024-12-10 00:34:30 +0100

    [Day 9] Preparations for Assignment 2

--- 9/day09/day09.go
+++ 9/day09/day09.go
@@ -12,23 +12,16 @@ type file struct {
 	size filesize
 }
 
-type filesystem storage
-
-type freespace file
-
 type filesize int
-
-type space file
 
 type storage []disk
 
-func SortStorage(sto storage) storage {
-	var sortedStorage storage
-	for i := range sto {
-		sortedStorage = append(sortedStorage, sortDisks(sto[i]))
-	}
-	return sortedStorage
+type swap struct {
+	file int
+	free int
 }
+
+type swaps []swap
 
 func SortStorageByFile(sto storage) storage {
 	var sortedStorage storage
@@ -39,61 +32,38 @@ func SortStorageByFile(sto storage) storage {
 }
 
 func sortFileSystem(d disk) disk {
-	for i := len(d) - 1; i >= 0; i-- {
-		for i2 := range d {
-			if d[i].size <= d[i2].size {
-				if d[i].size < d[i2].size {
-					difference := d[i2].size - d[i].size
-					temp := d[i]
-					temp2 := file{
-						id:   ".",
-						size: difference,
-					}
-					d = insertValues(d, i2, temp, temp2)
-
-					d[i2].size = d[i2].size - difference
-					d[i] = d[i2]
-
-				}
-				if d[i].size == d[i2].size {
-					temp := d[i]
-					d[i] = d[i2]
-					d[i2] = temp
-				}
-			}
+	swapInstructions := swaps{}
+	for i := len(d) - 1; i > 0; i-- {
+		if d[i].id != "." {
+			swapPartner := getForwardFreeSpaceForNeededSpace(d, d[i].size)
+			fmt.Println(d[i].id, "sp:", swapPartner)
+			swapInstructions = append(swapInstructions, swap{
+				file: i,
+				free: swapPartner,
+			})
 		}
 	}
-	d = RecalcFreeSpace(d)
-	return d
-}
 
-func insertValues(d disk, index int, files ...file) disk {
-	if index < 0 || index > len(d) {
-		panic("index out of range")
-	}
-	// Append the values at the specified index
-	return append(d[:index], append(files, d[index:]...)...)
-}
-
-func RecalcFreeSpace(d disk) disk {
-	var newD disk
-	for i := range d {
-		if d[i].id == "." {
-			if i != len(d)-1 {
-				if d[i+1].id == "." {
-					f := file{
-						id:   ".",
-						size: d[i].size + d[i+1].size,
-					}
-					newD = append(newD, f)
-					i++
+	for i := range swapInstructions {
+		if swapInstructions[i].free == -1 {
+			if swapInstructions[i].free != getForwardFreeSpaceForNeededSpace(d, filesize(swapInstructions[i].file)) {
+				if swapInstructions[i].free != -1 {
+					d = swapFileWithFreeSpace(d, swapInstructions[i].file, getForwardFreeSpaceForNeededSpace(d, filesize(swapInstructions[i].file)))
 				}
 			}
 		} else {
-			newD = append(newD, d[i])
+			d = swapFileWithFreeSpace(d, swapInstructions[i].file, swapInstructions[i].free)
 		}
 	}
-	return newD
+	return d
+}
+
+func SortStorage(sto storage) storage {
+	var sortedStorage storage
+	for i := range sto {
+		sortedStorage = append(sortedStorage, sortFileSystem(sto[i]))
+	}
+	return sortedStorage
 }
 
 func sortDisks(d disk) disk {
@@ -119,31 +89,6 @@ func CalculateStorageChecksum(sto storage) int {
 	sum := 0
 	for i := range sto {
 		sum += calculateDiskChecksum(sto[i])
-	}
-	return sum
-}
-
-func CalculateStorageByFileChecksum(sto storage) int {
-	sum := 0
-	for i := range sto {
-		sum += calculateDiskByFileChecksum(sto[i])
-	}
-	return sum
-}
-
-func calculateDiskByFileChecksum(d disk) int {
-	sum := 0
-	printDisk(d)
-	for i := range d {
-		if d[i].id != "." {
-			id, _ := strconv.Atoi(d[i].id)
-			for j := i; j < i+int(d[i].size); j++ {
-				result := j * id
-				sum += result
-				fmt.Println(j, "*", id, "=", result)
-			}
-			i = i + int(d[i].size)
-		}
 	}
 	return sum
 }
@@ -178,32 +123,47 @@ func isDiskSorted(d disk) bool {
 	return true
 }
 
-func ConverStorageRepresentationToFileRepresentation(sto storage) storage {
-	fs := storage{}
+func ConvertFileRepresentationToStorageRepresentation(sto storage) storage {
+	newStorage := storage{}
 	for i := range sto {
-		d := disk{}
-		currentIndex := ""
+		newDisk := disk{}
 		for j := range sto[i] {
-			currentIndex = sto[i][j].id
-			counter := 0
-			fmt.Println(currentIndex)
-			for i2 := range sto {
-				if sto[i][i2].id == currentIndex {
-					counter++
-				} else {
-					break
-				}
+			for k := 0; k < int(sto[i][j].size); k++ {
+				newDisk = append(newDisk, file{id: sto[i][j].id, size: 1})
 			}
-			if j != len(sto[i])-1 {
-				j += counter - 1
+		}
+		newStorage = append(newStorage, newDisk)
+	}
+	return newStorage
+}
+
+func ConvertStorageRepresentationToFileRepresentation(sto storage) storage {
+	fs := storage{}
+
+	for _, d := range sto {
+		newDisk := disk{}
+		i := 0
+		for i < len(d) {
+			currentID := d[i].id
+			counter := 1
+
+			// Count consecutive occurrences of the same id
+			for i+counter < len(d) && d[i+counter].id == currentID {
+				counter++
 			}
-			d = append(d, file{
-				id:   currentIndex,
+
+			// Append the file with id and its consecutive count
+			newDisk = append(newDisk, file{
+				id:   currentID,
 				size: filesize(counter),
 			})
+
+			// Move the index to the next different id
+			i += counter
 		}
-		fs = append(fs, d)
+		fs = append(fs, newDisk)
 	}
+
 	return fs
 }
 
@@ -234,18 +194,46 @@ func ConvertInputToStorageRepresentation(data [][]string) []disk {
 }
 
 func printDisk(d disk) {
-	fmt.Println("----------------Disk------------\n")
+	fmt.Println("----------------Disk---------------\n")
 	for i := range d {
 		fmt.Print(d[i].id)
 	}
-	fmt.Println("\n--------------------------------\n")
+	fmt.Println("\n-----------------------------------\n")
+}
+
+func swapFileWithFreeSpace(d disk, i, j int) disk {
+	thefile := d[i]
+	remainingSize := int(d[j].size) - int(d[i].size)
+	if remainingSize == 0 {
+		d[i] = thefile
+		d[i] = d[j]
+		d[j] = thefile
+	} else {
+		d[j].size = filesize(int(d[j].size) - remainingSize)
+		d[i] = d[j]
+		d[j] = thefile
+		d = append(d[:j+1], append([]file{file{
+			id:   ".",
+			size: filesize(remainingSize),
+		}}, d[j+1:]...)...)
+	}
+	return d
+}
+
+func getForwardFreeSpaceForNeededSpace(d disk, size filesize) int {
+	for i := range d {
+		if d[i].id == "." {
+			if d[i].size >= size {
+				return i
+			}
+		}
+	}
+	return -1
 }
 
 func PrintStorage(sto storage) {
 	for i := range sto {
-		for i2 := range sto[i] {
-			fmt.Print(sto[i][i2].id)
-		}
+		printDisk(sto[i])
 	}
 }
 
--- 9/main.go
+++ 9/main.go
@@ -9,16 +9,16 @@ import (
 func main() {
 	data, _ := util.ReadFile("/home/janneumann/Dokumente/Daten/Projekte/Privat/advent-of-code-2024/9/input.txt", "", true)
 	storage := day09.ConvertInputToStorageRepresentation(data)
-	fs := day09.ConverStorageRepresentationToFileRepresentation(storage)
+	fs := day09.ConvertStorageRepresentationToFileRepresentation(storage)
 	day09.PrintStorage(storage)
 	sortedStorage := day09.SortStorage(storage)
 	fmt.Println("-----------------------")
 	day09.PrintStorage(sortedStorage)
 	fmt.Println("\n++++++++++++++++++++++")
 	fmt.Println("Solution: ", day09.CalculateStorageChecksum(sortedStorage))
-	fmt.Println("\n++++++++++++++++++++++")
-	day09.PrintStorage(fs)
-	fmt.Println("\n", len(fs[0]))
-	fmt.Println("\n++++++++++++++++++++++")
-	fmt.Println("Solution: ", day09.CalculateStorageByFileChecksum(fs))
+
+	sortedFileStorage := day09.SortStorageByFile(fs)
+	day09.PrintStorage(sortedFileStorage)
+	newFS := day09.ConvertFileRepresentationToStorageRepresentation(sortedFileStorage)
+	fmt.Println("Solution: ", day09.CalculateStorageChecksum(newFS))
 }
